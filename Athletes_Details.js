54277ba08ea5a97ee7807f446b056a5871ceaff4
MarioSantoscv <mariosantos1@ua.pt>
2024-12-05 14:01:54 +0000

    Add files via upload

---
+++ Athletes_Details.js
@@ -0,0 +1,84 @@
+﻿
+var vm = function () {
+    console.log('ViewModel initiated...');
+
+    var self = this;
+
+
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/Api/Athletes/');
+    self.Id = ko.observable("Unavailable");
+    self.Name = ko.observable("Unavailable");
+    self.NameShort = ko.observable("Unavailable");
+    self.Photo = ko.observable("Unavailable");
+    self.Height = ko.observable("Unavailable");
+    self.Weight = ko.observable("Unavailable");
+    self.Function = ko.observable("Unavailable");
+    self.Reason = ko.observable("Unavailable");
+    self.Ritual = ko.observable("Unavailable");
+
+
+    self.Text = ko.computed(function () {
+        let txt =
+            "Id: " + self.Id() + "<hr/>" +
+            "Name: " + self.Name() + "<hr/>" +
+            "Name (Short): " + self.NameShort() + "<hr/>" +
+            "Height: " + self.Height() + "<hr/>" +
+            "Weight: " + self.Weight() + "<hr/>" +
+            "Function: " + self.Function() + "<hr/>" +
+            "Reason: " + self.Reason() + "<hr/>" +
+            "Ritual: " + self.Ritual() + "<hr/>";
+            
+        return txt;
+    });
+
+
+    self.active = function (id) {
+        let composedUri = self.baseUri() + id;
+        console.log("Accessing: " + composedUri);
+        self.Id(id);
+
+
+        $.ajax({
+            type: "GET",
+            url: composedUri,
+            dataType: "json",
+            success: function (data) {
+
+                self.Name(data.Name);
+                self.NameShort(data.NameShort);
+                self.Photo(data.Photo);
+                self.Height(data.Height);
+                self.Weight(data.Weight);
+                self.Function(data.Function);
+                self.Reason(data.Reason);
+                self.Ritual(data.Ritual);
+                self.Sport(data.Sport);
+                console.log(data);
+            },
+            error: function () {
+                console.error("Data not found!");
+            },
+        });
+    };
+
+
+    function getPage() {
+        var currentUrl = String(window.location.search);
+        let parameters = currentUrl.split("?")[1]?.split("&");
+        if (parameters && parameters[0]) {
+            let id = parameters[0].split("=")[1];
+            console.log("ID extracted from URL: " + id);
+            self.active(id);
+        } else {
+            console.error("No ID found in the URL.");
+        }
+    }
+
+
+    getPage();
+};
+
+$(document).ready(function () {
+    console.log("document.ready!");
+    ko.applyBindings(new vm());
+});
---
+++ myJs.js
@@ -0,0 +1,141 @@
+﻿
+//Controls the theme(made by teacher)
+(() => {
+    'use strict'
+
+    const getStoredTheme = () => localStorage.getItem('theme')
+    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = theme => {
+        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+    }
+
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector('#bd-theme')
+
+        if (!themeSwitcher) {
+            return
+        }
+
+        const themeSwitcherText = document.querySelector('#bd-theme-text')
+        const activeThemeIcon = document.querySelector('.theme-icon-active')
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.add('opacity-50');
+            element.setAttribute('aria-pressed', 'false');
+        })
+
+        //btnToActive.classList.add('active');
+        btnToActive.setAttribute('aria-pressed', 'true');
+        btnToActive.classList.remove('opacity-50')
+
+        if (focus) {
+            themeSwitcher.focus()
+        }
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    setStoredTheme(theme)
+                    setTheme(theme)
+                    showActiveTheme(theme, true)
+                })
+            })
+    })
+})()
+
+
+
+
+//At start, verify if there are any favorites and then set the hearts
+let SetHearths = function (list) {
+    $("#place")
+        .children()
+        .each((index, element) => {
+            let dataId = element.id;
+
+            list.forEach((item) => {
+                if (item.Id == dataId) {
+                    let iElement = $(element).find(".bd .cont i");
+                    iElement.removeClass("fa-heart-o");
+                    iElement.addClass("fa-heart");
+                    iElement.addClass("text-danger");
+                }
+            });
+        });
+};
+
+//Remove a favorite from the list and update the viewmodel
+let removeFav = function (data, $currentTarget) {
+    $currentTarget.parent().remove();
+    $("#place")
+        .children()
+        .each((index, element) => {
+            if (element.id == data.Id) {
+                let iElement = $(element).find(".bd .cont i");
+                iElement.removeClass("fa-heart");
+                iElement.removeClass("text-danger");
+                iElement.addClass("fa-heart-o");
+            }
+        });
+    manageFavs(data);
+};
+
+//Changes the heart icon and calls the manageFavs function
+let swapFav = function (element, data2,) {
+    if (localStorage.getItem(favType) != null) {
+        fav = JSON.parse(localStorage.getItem(favType));
+    }
+    let heart = $(element).find("i");
+
+    heart.toggleClass("text-danger");
+    heart.toggleClass("fa-heart");
+    heart.toggleClass("fa-heart-o");
+
+    manageFavs(data2);
+};
+
+//Add or remove a favorite from the list and save it to local storage
+let manageFavs = function (data2) {
+    let dataId = data2.Id;
+    if (fav.some((item) => item.Id === dataId)) {
+        fav = fav.filter((item) => item.Id !== dataId);
+    } else {
+        fav.push(data2);
+    }
+
+    localStorage.setItem(favType, JSON.stringify(fav));
+
+    // checks the correct context that can be either a page that shows the favorites or where u can only add it
+    if (typeof VM.favList !== "undefined") {
+        VM.favList(fav);
+    }
+};

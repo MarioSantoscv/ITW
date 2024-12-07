49ae6e024dcb2422586f18291d5817742937e065
Mario Lucas <mariosantos1@ua.pt>
2024-12-07 17:58:23 +0000

    0

--- Treinadores_Details.js
+++ Treinadores_Details.js
@@ -12,8 +12,8 @@ var vm = function () {
     self.Photo = ko.observable("Unavailable");
     self.Sex = ko.observable("Unavailable");
     self.Function = ko.observable("Unavailable");
-    self.Country_Code = ko.observable("Unavailable");
-    self.Sports = ko.observable("Unavailable");
+    self.Country = ko.observable("Unavailable");
+    self.Sports = ko.observableArray([]);
     self.BirthDate = ko.observable("Unavailable");
    
 
@@ -24,8 +24,7 @@ var vm = function () {
             "Name: " + self.Name() + "<hr/>" +
             "Sex: " + self.Sex() + "<hr/>" +
             "Function: " + self.Function() + "<hr/>" +
-            "Country_Code: " + self.Country_Code() + "<hr/>" +
-            "Sports: " + self.Sports() + "<hr/>" +
+            "Country: " + self.Country() + "<hr/>" +
             "BirthDate: " + self.BirthDate() + "<hr/>";
 
         return txt;
@@ -46,16 +45,12 @@ var vm = function () {
                 self.Id(data.Id)
                 self.Name(data.Name);
                 self.Sex(data.Sex);
-                if (data.Photo) {
-                    self.Photo(data.Photo);
-                } else {
-                    self.Photo("C:\Users\santo\source\repos\Projeto final\wwwroot\imagens\Carousel4.png");  //ask teacher abt picture
-                }
-                
+                self.Photo(data.Photo);
                 self.Function(data.Function);
-                self.Sports(data.Sport);
-                self.Country_Code(data.Country_Code);
+                self.Country(data.Country);
                 self.BirthDate(data.BirthDate);
+                const sports = data.Sports.map(sport => sport.Name);
+                self.Sports(sports);
                 console.log(data);
             },
             error: function () {
